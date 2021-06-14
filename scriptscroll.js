d7ea6c1e0945e0d5d24f54b1e8d4fe860d300a09
AGregg <60059030+AGreggStudio@users.noreply.github.com>
2021-06-14 20:31:22 +1000

    js and css

---
+++ scriptscroll.js
@@ -0,0 +1,76 @@
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+	let items = document.querySelectorAll('section');
+	for (let i = 0; i < items.length; i++){
+		items[i].style.background = randomColor({white});
+	}
+	cssScrollSnapPolyfill()
+}
+init();
+
+
+
+
+function loop ($swap) {
+  var next = $swap.find("li.visible").removeClass("visible").index() + 1;
+  
+  if(next >= $swap.find("li").length) {
+    next = 0;
+  }
+  
+  $swap.width($($swap.find("li").get(next)).addClass("visible").outerWidth());
+  $swap.css({ "transform" : "translateY(-" + next * 3 + "rem)" });
+  
+  setTimeout(function () { loop($swap); }, 2000);
+};
+
+$(function () {
+  $(".swap").each(function () {
+    var $this = $(this);
+    
+    $this.find("li").each(function () {
+      $(this).css({ top: $(this).index() * 3 + "rem" });
+    });
+    
+    loop($this);
+  });
+});
+
+(function() {
+  var follower, init, mouseX, mouseY, positionElement, printout, timer;
+
+  follower = document.getElementById('follower');
+
+  printout = document.getElementById('printout');
+
+  mouseX = (event) => {
+    return event.clientX;
+  };
+
+  mouseY = (event) => {
+    return event.clientY;
+  };
+
+  positionElement = (event) => {
+    var mouse;
+    mouse = {
+      x: mouseX(event),
+      y: mouseY(event)
+    };
+    follower.style.top = mouse.y + 'px';
+    return follower.style.left = mouse.x + 'px';
+  };
+
+  timer = false;
+
+  window.onmousemove = init = (event) => {
+    var _event;
+    _event = event;
+    return timer = setTimeout(() => {
+      return positionElement(_event);
+    }, 1);
+  };
+
+}).call(this);
